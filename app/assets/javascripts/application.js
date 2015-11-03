faa49b4a45733e6128dfd1dcb3c0485537aa1918
Alejandro Pereira <ja.pereiragt@gmail.com>
2015-11-03 08:43:31 -0600

    Our first Controller

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require jquery
-//= require bootstrap-sprockets
+//= require bootstrap
 //= require jquery_ujs
-//= angular
+//= require angular
 //= require_tree .
